57ecdfd59af2630b1e2732f601bd398d735f0226
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-20 12:51:21 -0500

    Added a function that will open the inventory as a dictionary

--- gas_core.py
+++ gas_core.py
@@ -1,12 +1,17 @@
+from disk import subtracting_tank, load_inventory
 def price_of(inventory, gas_name):
     ''' ([[str, float, float]], str) _> (float)
     This function will get a string and 
     it will look in a txt file and pull out 
     the price of that gas and return it
     ''' 
+    new_dict = {}
     for item in inventory:
         if gas_name.strip() == item[0].strip().lower():
             return float(item[1])
+
+
+
 def calc_gallon(money, price_gas):
     """ (float, float) _> float
     This funciton will get the amount of money
@@ -52,3 +57,19 @@ def other_message(name, price, gallons):
     # For paying after
     """ str, float, float -> str """
     return 'Your total is ${:0.2f} of {} gas for {} gallons.'.format(gallons, name, price)
+
+
+
+def manager_inventory():
+    ''' _> dict{dict{}}
+    Fuction will load the prices as a dictionaries of 
+    dictionaries.
+    '''
+    inventory = {}
+    with open('prices.txt', 'r') as file:
+        _, key_1, key_2 = file.readline().strip().split(', ')
+        lines = file.readlines()
+        for line in lines:
+            gas_type, price, gallons = line.strip().split(', ')
+            inventory[gas_type] = {key_1: float(price), key_2: float(gallons)}
+        return inventory
