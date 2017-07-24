cdc6971d043d89c5bf4b72bf175d8e53e4eec094
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-24 09:30:47 -0500

    Added some branches

--- manger_shell.py
+++ manger_shell.py
@@ -1,4 +1,10 @@
 from gas_core import manager_inventory
-print("Hello manager!")
+from disk import subtracting_tank
+print("Hello manager, here is our inventory")
 inventory = manager_inventory()
 print(inventory)
+print('What would you like to do today?')
+print('1.\tRefuel\n2.\tCheck the gallons bought for a specefic gas type\n3.\tCheck the amount of gas bought for the day')
+answer = input().strip()
+# if answer == '1':
+
