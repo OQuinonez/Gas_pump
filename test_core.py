f5cb898aeea12ea388af11701d95f3083edd2cac
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-24 11:25:57 -0500

    Broke the test and fixed it to make sure it works

--- test_core.py
+++ test_core.py
@@ -7,3 +7,14 @@ def test_calc_money():
     assert gas_core.calc_money(1, 2.07) == 2.07
     assert gas_core.calc_money(20, 2.07) == 41.4
     assert gas_core.calc_money(10, 2.07) == 20.7
+
+def test_final_message():
+    actual =  gas_core.final_message('regular', 2.05, 12)
+    expected = 'You bought 12.00 gallons of regular gas for $2.05.'
+    assert actual == expected
+
+# def test_other_message():
+
+#     # For paying after
+#     """ str, float, float -> str """
+#     return 'Your total is ${:0.2f} of {} gas for {} gallons.'.format(gallons, name, price)
