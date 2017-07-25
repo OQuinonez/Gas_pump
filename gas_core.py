67e8186e04e28d0d6f45ee19bcf80a530ba485a0
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2017-07-25 09:15:46 -0500

    Moved a function to the disk

--- gas_core.py
+++ gas_core.py
@@ -39,33 +39,11 @@ def calc_money(gallons, price_gas):
     answer = (gallons * price_gas)
     return answer
 
-
-def update_inventory(type_gas, gallons, price):
-    ''' str, float, float _> None
-    '''
-    msg = type_gas + ', ' + str(gallons) + ', ' + str(price) + '\n'
-    with open('gas.txt', 'a') as file:
-        file.write(msg)
-    return None
 def final_message(name, price, gallons):
     # for prepay
     """ str, float, float -> str """
     return 'You bought {:0.2f} gallons of {} gas for ${:0.2f}.'.format(gallons, name, price)
 def other_message(name, price, gallons):
-
     # For paying after
     """ str, float, float -> str """
     return 'Your total is ${:0.2f} of {} gas for {} gallons.'.format(gallons, name, price)
-def manager_inventory():
-    ''' _> dict{dict{}}
-    Fuction will load the prices as a dictionaries of 
-    dictionaries.
-    '''
-    inventory = {}
-    with open('prices.txt', 'r') as file:
-        _, key_1, key_2, key_3 = file.readline().strip().split(', ')
-        lines = file.readlines()
-        for line in lines:
-            gas_type, price, gallons, code = line.strip().split(', ')
-            inventory[gas_type] = {key_1: float(price), key_2: float(gallons), key_3: str(code)}
-        return inventory
